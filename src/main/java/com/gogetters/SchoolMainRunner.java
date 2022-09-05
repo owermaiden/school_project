d6c384377a175a6e58b6b659bb5f617527a13a4b
Dilbar <dibux9@gmail.com>
2022-09-04 22:49:00 -0400

    CourseService class is tested in SchoolMainRunner

--- src/main/java/com/gogetters/SchoolMainRunner.java
+++ src/main/java/com/gogetters/SchoolMainRunner.java
@@ -1,11 +1,14 @@
 package com.gogetters;
 
+import com.gogetters.database.Database;
 import com.gogetters.entity.Course;
 import com.gogetters.entity.Parent;
 import com.gogetters.entity.School;
 import com.gogetters.entity.Student;
+import com.gogetters.enums.WeekDays;
 import com.gogetters.service.*;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,21 +16,25 @@ import java.util.Scanner;
 public class SchoolMainRunner {
 
 //    public static CRUDService<School> schoolService;
-//    public static CRUDService<Course> courseService;
+    public static CRUDService<Course> courseService;
 //    public static CRUDService<Parent> parentService;
 //    public static CRUDService<Student> studentService;
 //
-//    static {
+    static {
 //        SchoolMainRunner.schoolService = new SchoolService();
-//        SchoolMainRunner.courseService = new CourseService();
+        SchoolMainRunner.courseService = new CourseService();
 //        SchoolMainRunner.parentService = new ParentService();
 //        SchoolMainRunner.studentService = new StudentService();
-//    }
-
-    public static void main(String[] args) {
-
-
     }
 
+    public static void main(String[] args) {
+        Course course = new Course(40,"HTML",90,List.of(WeekDays.THURSDAY));
+        courseService.save(course);
+        System.out.println(Database.courseList);
+
+        Course course1 = new Course(40,"HTML",80,List.of(WeekDays.THURSDAY,WeekDays.WEDNESDAY));
+        courseService.update(course1);
+        System.out.println(Database.courseList);
+    }
 
 }
